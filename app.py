0dc231e8a0d5b34e6485bbb28e5177bc29a7b2f7
ARAVIND1408 <aravind14082002@gmail.com>
2025-04-30 13:57:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,5 +73,8 @@ def api_water_consumption():
         return jsonify({'error': 'No data available', 'details': 'Airtable returned no records'}), 404
     return jsonify(data)
 
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
